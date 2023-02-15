61e575d65971b00e8b2362b27a44b61632029518
Claire Walker <claiwalk@iu.edu>
2023-02-15 13:54:49 -0500

    Update and rename RockPaperScissors to RockPaperScissors.py

---
+++ RockPaperScissors.py
@@ -0,0 +1,18 @@
+# import random
+import random
+
+# define list
+plays = ["rock", "paper", "scissors"]
+
+# define user input
+userChoice = input("Please choose 'rock', 'paper', or 'scissors': ")
+
+# define computer random output
+computerChoice = random.choice(plays)
+
+# define rules
+
+
+# create output for 'game'
+
+
