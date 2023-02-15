c4867e3cf979337fcdad843e8ab301a4a744e833
Claire Walker <claiwalk@iu.edu>
2023-02-15 14:06:59 -0500

    Update RockPaperScissors.py

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -5,14 +5,31 @@ import random
 plays = ["rock", "paper", "scissors"]
 
 # define user input
+print("Let's play rock, paper, scissors!")
 userChoice = input("Please choose 'rock', 'paper', or 'scissors': ")
 
 # define computer random output
 computerChoice = random.choice(plays)
 
 # define rules
-
-
-# create output for 'game'
+if computerChoice = "rock":
+  if userChoice = "paper":
+    print("Rock beats paper! The computer wins!")
+  if userChoice = "scissors":
+    print("Scissors beats rock! You win!")
+  if userChoice = "rock":
+    print("It's a tie! Try again!")
+  else:
+    print("Sorry, I couldn't understand what you wrote. Please try again and type 'rock', 'paper, or 'scissors'."
+        
+if computerChoice = "paper":
+  if userChoice = "scissors":
+    print("Rock beats paper! The computer wins!")
+  if userChoice = "scissors":
+    print("Scissors beats rock! You win!")
+  if userChoice = "rock":
+    print("It's a tie! Try again!")
+  else:
+    print("Sorry, I couldn't understand what you wrote. Please try again and type 'rock', 'paper, or 'scissors'.")
 
 
